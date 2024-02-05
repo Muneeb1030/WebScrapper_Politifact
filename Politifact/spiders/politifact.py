aae3c1477aea3ec4f9e94a3542fb95322f8abb6b
Muhammad Muneeb ur Rehman <m.muneeb.ur.rehman.2000@gmail.com>
2024-02-06 03:10:49 +0500

    Scrapping Individual Article Done

--- Politifact/spiders/politifact.py
+++ Politifact/spiders/politifact.py
@@ -18,7 +18,6 @@ class PolitifactSpider(Spider):
     Ruling = []
     Publisher = []
     ArticleUrl = []
-    #later will be used 
     ArticleTags = []
     ArticleShortResponse = []
     ArticleShortDiscussion = []
@@ -72,5 +71,39 @@ class PolitifactSpider(Spider):
         pass
     
     def DownloadContentInPost(self, response):
-        pass
+        title = response.meta['statement_text']
+        
+        statement_tags = response.xpath('//*[@class="m-statement__content"]//*[@class="m-list__item"]/a/@title').extract()
+        statement_tags = ', '.join(statement_tags)
+        
+        statement_breif_response = response.xpath('//*[@class="t-row__center"]/header/h2/text()').extract_first().strip()
+         
+        statement_short_Discussion_Header = response.xpath('//*[@class="t-row__center"]//*[@class="m-callout__title"]/text()').extract_first().strip()
+        statement_short_discussion = response.xpath('//*[@class="t-row__center"]//*[@class="m-callout__body"]//li//text()').extract()
+        statement_short_discussion = [stmnt.strip() for stmnt in statement_short_discussion if stmnt.strip()]
+        # Convert the final list to a single string with counts
+        output_string = ', '.join(f"{index + 1}. {stmnt}" for index, stmnt in enumerate(statement_short_discussion))
+        statement_short_Discussion_Footer = response.xpath('//*[@class="t-row__center"]//*[@class="m-callout__link"]/a/text()').extract_first()
+        
+        statment_discussed = response.xpath('//*[@class="t-row__center"]//*[@class="m-textblock"]//p//text()').extract()
+        statment_discussed = [stmnt for stmnt in statment_discussed if stmnt.strip()]
+        statment_discussed = [stmnt.strip() for stmnt in statment_discussed]
+        statment_discussed = ' '.join(statment_discussed)
+        
+        sourcesHeader = response.xpath('//*[@class="t-row__center"]//*[@class="m-superbox__title"]/text()').extract_first()
+        
+        statement_facts = response.xpath('//*[@class="t-row__center"]//*[@class="m-superbox__content"]/p//text()').extract()
+        statement_facts = [stmnt.strip() for stmnt in statement_facts]
+        statement_facts = [stmnt for stmnt in statement_facts if stmnt.strip()]
+        statement_facts =  ' '.join(statement_facts)
+        sentences = re.split(r'(?<=\b(?:19|20)\d{2}\b)\s+', statement_facts)
+        statement_facts = ' '.join(sentences)
+        
+        self.ArticleTags.append(statement_tags)
+        self.ArticleShortResponse.append(statement_breif_response)
+        self.ArticleShortDiscussion.append(output_string)
+        self.ArticleDiscussion.append(statment_discussed)
+        self.ArticleSources.append(statement_facts)
+            
+
           
